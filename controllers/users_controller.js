05921642169fda2398e9026d51ff9c59214e2a4a
Sanjay Kumar Meena <s.k.meena200@gmail.com>
2022-04-15 01:07:31 +0530

    manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User = require('../models/users');
 
 module.exports.profile = function(req, res) {
-    return res.render('user_profile', {
-        title: "User Profile"
-    });
+    if(req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function(err, user) {
+            if(user) {
+                return res.render('user_profile', {
+                    title: "User Profile",
+                    user: user
+                });
+            }
+            
+            return res.redirect('/users/sign-in');
+        });
+    }
+    else {
+        return res.redirect('/users/sign-in');
+    }
 }
 
 
@@ -21,6 +33,14 @@ module.exports.signIn = function(req, res) {
         title: "Etilaicos | Sign In"
     })
 }
+
+
+// sign out
+module.exports.signOut = function(req, res) {
+   res.cookie('user_id', '', { maxAge: 1} );
+   return res.redirect('/users/sign-in');
+}
+
 
 // get the signup data
 module.exports.create = function(req, res) {
@@ -45,7 +65,30 @@ module.exports.create = function(req, res) {
 }
 
 
+
 // sign in and create the session for the user
 module.exports.createSession = function(req, res) {
-    // TODO later
+
+    // steps to authenticate
+    // find the user
+    User.findOne({emial: req.body.email}, function(err, user) {
+        if(err) { console.log('error in finding user in signing in'); return; }
+
+        // handle user found
+        if(user) {
+            // handle password which doesn't match
+            if(user.password != req.body.password) {
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }
+        else {
+            // handle user not found
+            return res.redirect('back');
+        }
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,10 @@ router.get('/profile', usersController.profile);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
+router.get('/sign-out', usersController.signOut);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 
 module.exports = router;

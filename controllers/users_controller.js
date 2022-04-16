2a0fe51060a05148a6ebf219837eb8aa0f78492f
Sanjay Kumar Meena <s.k.meena200@gmail.com>
2022-04-17 00:14:49 +0530

    Authentication added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,14 +1,20 @@
-const User = require('../models/users');
+const User = require('../models/user');
 
-module.exports.profile = function(req, res) {
+
+module.exports.profile = function(req, res){
     return res.render('user_profile', {
-        title: "User Profile"
-    });
+        title: 'User Profile'
+    })
 }
 
 
 // render the sign up page
-module.exports.signUp = function(req, res) {
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up', {
         title: "Etilaicos | Sign Up"
     })
@@ -16,36 +22,46 @@ module.exports.signUp = function(req, res) {
 
 
 // render the sign in page
-module.exports.signIn = function(req, res) {
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Etilaicos | Sign In"
     })
 }
 
-// get the signup data
-module.exports.create = function(req, res) {
-    if(req.body.password != req.body.confirm_password) {
-        return res.redirect('back'); 
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user) {
-        if(err) { console.log('error in finding user in signing up'); return; }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
 
-        if(!user) {
-            User.create(req.body, function(err, user) {
-                if(err) { console.log('error in creating user while signing up'); return; }
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
 
                 return res.redirect('/users/sign-in');
             })
-        }
-        else {
+        }else{
             return res.redirect('back');
         }
+
     });
 }
 
 
-// sign in and create the session for the user
-module.exports.createSession = function(req, res) {
-    // TODO later
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -2,25 +2,72 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
-
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+// const sassMiddleWare = require('node-sass-middleware');
+
+
+// app.use(sassMiddleWare({
+//     src: './assets/scss',
+//     dest: './assets/css',
+//     debug: true,
+//     outputStyle: 'expanded',
+//     prefix: '/css'
+// }));
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
-app.use(express.urlencoded());
-app.use(cookieParser());
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'etilaicos',
+    // TODO change the secret befor deployment in production mode
+    secret:'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            mongoUrl: 'mongodb://localhost/etilaicos',
+            autoRemove: 'disabled'
+        },
+        function(err) {
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(passport.setAuthenticatedUser);
+
+
+// use express router
+app.use('/', require('./routes'));
 
 
 app.listen(port, function(err) {

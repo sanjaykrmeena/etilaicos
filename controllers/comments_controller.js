ddc033af71a188282b8aedb56c6bbbb88456210d
Sanjay Kumar Meena <s.k.meena200@gmail.com>
2022-05-19 23:12:03 +0530

    added delete option on posts and comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,7 +3,6 @@ const Post = require('../models/post');
 
 module.exports.create = function(req, res) {
     Post.findById(req.body.post, function(err, post) {
-
         if(post) {
             Comment.create({
                 content: req.body.content,
@@ -20,5 +19,25 @@ module.exports.create = function(req, res) {
             });
         }
 
+    });
+}
+
+
+module.exports.destroy = function(req, res) {
+    Comment.findById(req.params.id, function(err, comment) {
+        if(comment.user == req.user.id) {
+
+            let postId = comment.post;
+
+            comment.remove();
+
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post) {
+                return res.redirect('back');
+            });
+        }
+        else {
+            return res.redirect('back');
+        }
     });
 }
